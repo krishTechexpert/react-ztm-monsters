d7d9587af0d1242ad6baaa0f97139d3b7f0dc1c1
krish <krishcode9@gmail.com>
2024-12-20 22:55:13 +0530

    class component setState method asynchronous

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react';
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      name:{fn:'krish',ln:'kumar'},
+      company:'google'
+    }
+  }
+render(){
+  console.log('render')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      hello,{this.state.name.fn} {this.state.name.ln}  and I worked at {this.state.company.ln}
+      <button onClick={() => {
+        this.setState(() => {
+          return {
+            name:{fn:'html',ln:'css'}
+          }
+        },() => console.log(this.state))// asynschronus
+      console.log(this.state)
+      }}>change name</button>
     </>
   )
+}
+  
 }
 
 export default App
